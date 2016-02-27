7c0883e9ea8cfddcd6ba7bb2b35a01cdfb03948a
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-02-26 17:45:41 -0700

    Optimize 15

--- 15/Fifteen.scala
+++ 15/Fifteen.scala
@@ -1,10 +1,10 @@
 object Fifteen {
-    // This isn't very efficient at all, but it felt good to write.
-    def pascal(row: Int, col: Int): BigInt = (row, col) match {
-        case (1, col) => BigInt(1)
-        case (row, 1) => BigInt(1)
-        case (2, col) => BigInt(col)
-        case (row, 2) => BigInt(row)
+    // This isn't very efficient at all, but it felt very Scala to write.
+    def pascal(row: Int, col: Int): Long = (row, col) match {
+        case (1, col) => 1.toLong
+        case (row, 1) => 1.toLong
+        case (2, col) => col.toLong
+        case (row, 2) => row.toLong
         case _        => {
             if (row == col)
                 pascal(row - 1, col) * 2
@@ -13,7 +13,20 @@ object Fifteen {
         }
     }
 
+    def pascal2(size: Int): Long = {
+        var matrix = Array.fill[Long](size, size)(1)
+        var x = 0
+        var y = 0
+
+        for (x <- 1 to (size - 1); y <- 1 to (size - 1)) {
+            matrix(x)(y) = matrix(x - 1)(y) + matrix(x)(y - 1)
+        }
+
+        matrix(size - 1)(size - 1)
+    }
+
     def main(args: Array[String]): Unit = {
-        println(pascal(21, 21))
+        // println(pascal(21, 21))
+        println(pascal2(21))
     }
 }
